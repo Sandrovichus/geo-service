fc74051082773cc3193b929265005eae5bbf5598
Sandrovichus <veber21@rambler.ru>
2023-07-18 23:05:58 +0300

    Maven

--- src/test/java/ru/netology/geo/GeoServiceTest.java
+++ src/test/java/ru/netology/geo/GeoServiceTest.java
@@ -1,9 +1,10 @@
+package ru.netology.geo;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
-import ru.netology.geo.GeoService;
-import ru.netology.geo.GeoServiceImpl;
+
 
 public class GeoServiceTest {
     @Test
--- src/test/java/ru/netology/i18n/LocalizationServiceTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceTest.java
@@ -1,8 +1,9 @@
+package ru.netology.i18n;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
-import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImpl;
+
 
 public class LocalizationServiceTest {
     @Test
